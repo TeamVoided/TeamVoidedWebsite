dea71a2f102b70239e0e4244b0ea1f533fff6a57
theendercore <theendercore@gmail.com>
2023-11-13 17:37:44 +0200

    - made things even better

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 	return (
 		<div class="flex h-[90vh] flex-col items-center justify-center gap-8">
 			<img src={logo} alt="TeamVoided logo" width="800" />
-			<h1 class="text-xl font-light italic">"The best god dam space pirates you have ever seen!"</h1>
+			<h1 class="text-xl font-light italic">"The best god dam space crab pirates you have ever seen!"</h1>
 		</div>
 	);
 }
